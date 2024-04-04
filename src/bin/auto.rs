20282d9d810508075fd78a252e39364dc40d9f0e
motii <108280115+motii8128@users.noreply.github.com>
2024-04-04 21:33:39 +0900

    Add files via upload

--- src/bin/auto.rs
+++ src/bin/auto.rs
@@ -9,6 +9,7 @@ use safe_drive::{
 use async_std;
 use serialport;
 use zikoichi_controller::Wheel;
+use ros2_rust_util::get_str_parameter;
 
 #[async_std::main]
 async fn main()->Result<(), DynError>
@@ -16,10 +17,13 @@ async fn main()->Result<(), DynError>
     let ctx = Context::new()?;
     let node = ctx.create_node("ZikoichiAuto", None, Default::default())?;
     let mut turtle_sub = node.create_subscriber::<nav_msgs::msg::Odometry>("/turtle1/odom", None)?;
-    let mut imu_sub = node.create_subscriber::<geometry_msgs::msg::Vector3>("/euler", None)?;
+    let mut euler_sub = node.create_subscriber::<geometry_msgs::msg::Vector3>("/euler", None)?;
+    let mut i_odom_sub = node.create_subscriber::<nav_msgs::msg::Odometry>("/imu/odom", None)?;
     let log = Logger::new(node.get_name());
 
-    let mut port = serialport::new("/dev/ttyACM0", 115200).timeout(std::time::Duration::from_millis(100)).open().unwrap();
+    let port_name = get_str_parameter(node.get_name(), "port_name", "/dev/ttyACM0");
+
+    let mut port = serialport::new(port_name, 115200).timeout(std::time::Duration::from_millis(100)).open().unwrap();
 
 
     let diagonal = ((2.0_f32).sqrt() / 2.0) as f64;
@@ -28,20 +32,21 @@ async fn main()->Result<(), DynError>
 
     loop {
         let t_odom = turtle_sub.recv().await.unwrap();
-        let imu_odom = imu_sub.recv().await.unwrap();
+        let imu_euler = euler_sub.recv().await.unwrap();
+        let imu_odom = i_odom_sub.recv().await.unwrap();
 
-        let imu_yaw = imu_odom.z;
+        let imu_yaw = imu_euler.z;
         let t_yaw = t_odom.pose.pose.position.z;
 
         pr_info!(log, "imu:{}, tur:{}", imu_yaw, t_yaw);
 
-        // let x_vec = t_odom.pose.pose.position.x - imu_odom.pose.pose.position.x;
-        // let y_vec = t_odom.pose.pose.position.y - imu_odom.pose.pose.position.y;
+        let x_vec = t_odom.pose.pose.position.x - imu_odom.pose.pose.position.x;
+        let y_vec = t_odom.pose.pose.position.y - imu_odom.pose.pose.position.y;
         let ro_vec = t_yaw - imu_yaw;
 
 
-        let x = 0.0;
-        let y = 0.0;
+        let x = control_vec(x_vec);
+        let y = control_vec(y_vec);
         let ro = control_vec(ro_vec);
 
         let mut cmd = Wheel::new(0.0, 0.0, 0.0, 0.0);
--- src/bin/manual.rs
+++ src/bin/manual.rs
@@ -9,6 +9,7 @@ use safe_drive::{
 use async_std;
 use serialport;
 use zikoichi_controller::Wheel;
+use ros2_rust_util::{get_str_parameter, get_f64_parameter};
 
 #[async_std::main]
 async fn main()->Result<(), DynError>
@@ -18,7 +19,10 @@ async fn main()->Result<(), DynError>
     let mut subscriber = node.create_subscriber::<geometry_msgs::msg::Twist>("/cmd_vel", None)?;
     let log = Logger::new(node.get_name());
 
-    let mut port = serialport::new("/dev/ttyACM0", 115200).timeout(std::time::Duration::from_millis(100)).open().unwrap();
+    let port_name = get_str_parameter(node.get_name(), "port_name", "/dev/ttyACM0");
+    let rate = get_f64_parameter(node.get_name(), "power_rate", 1.0);
+
+    let mut port = serialport::new(port_name, 115200).timeout(std::time::Duration::from_millis(100)).open().unwrap();
 
     let diagonal = ((2.0_f32).sqrt() / 2.0) as f64;
 
@@ -37,6 +41,11 @@ async fn main()->Result<(), DynError>
         cmd.front_right = -x * diagonal - y * diagonal + 0.5 * ro;
         cmd.rear_left = x * diagonal + y * diagonal + 0.5 * ro;
         cmd.rear_right = - x * diagonal + y * diagonal + 0.5 * ro;
+
+        cmd.front_left *= rate;
+        cmd.front_right *= rate;
+        cmd.rear_left *= rate;
+        cmd.rear_right *= rate;
 
         match port.write(cmd.serialize().as_bytes()) {
             Ok(_)=>{
